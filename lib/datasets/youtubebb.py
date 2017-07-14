92298654cb23a205fd705797f3a983f880bc1f3c
Mark Buckler <markabuckler@gmail.com>
2017-07-13 20:35:47 -0400

    Add in car class

--- lib/datasets/youtubebb.py
+++ lib/datasets/youtubebb.py
@@ -39,7 +39,7 @@ class youtubebb(imdb):
                          'bear','cow','cat','giraffe','potted plant',
                          'horse','motorcycle','knife','airplane',
                          'skateboard','train','truck','zebra','toilet',
-                         'dog','elephant','umbrella')
+                         'dog','elephant','umbrella','car')
         self._class_to_ind = dict(zip(self.classes, xrange(self.num_classes)))
         self._image_ext = '.jpg'
         self._image_index = self._load_image_set_index()
